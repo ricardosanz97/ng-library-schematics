9117fcc07bb9d4d9fe7d0e6b06ff8eb3ebeaaa2e
Ricardo Sanz <ricardosanz@quibim.com>
2023-01-04 10:10:03 +0100

    Add css assets to library

--- projects/ricardosanz97/ng-lib/src/lib/ng-lib.component.ts
+++ projects/ricardosanz97/ng-lib/src/lib/ng-lib.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-ng-lib',
-  template: ` <p>ng-lib works!!!!!!!!</p> `,
+  template: `
+    <p>
+      ng-lib-works!
+      <img class="avatar" src="assets/avatar-1.png" />
+    </p>
+  `,
   styles: [],
 })
 export class NgLibComponent implements OnInit {
